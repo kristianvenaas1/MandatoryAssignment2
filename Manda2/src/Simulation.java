e05a4d453d2f60ac4753473fdb06e12445a39470
kristianvenaas1 <89575807+kristianvenaas1@users.noreply.github.com>
2022-03-06 22:07:43 +0100

    Delete Manda2 directory

--- Manda2/src/Simulation.java
+++
@@ -1,9 +0,0 @@
-public class Simulation {
-    public static void main(String[] args) {
-        System.out.println("SUPER MARKET SIMULATOR:");
-        Supermarket supermarket = new Supermarket("SuperMarket_1");
-        supermarket.start();//Starts main thread: the supermarket
-        /**********************************************************************/
-        System.out.println("\t\t\tFinishing main thread...");
-    }
-}
